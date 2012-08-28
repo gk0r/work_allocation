e3a1d9331fa1cf0900c5d22137f7978f685af5ec
gk0r <gk0r.go@gmail.com>
2012-08-28 11:47:52 +1000

    Updated Index Heading and Index Tables to use a single style for easy manipulation via less mixins

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,11 @@
 module ApplicationHelper
+  
   def index_link(link_text, link_url)
     link_to link_text, link_url if link_text
   end
   
   def delete_link (path)
-    link_to '<i class="icon-white icon-trash"></i>'.html_safe, path, :confirm => 'Are you sure?', :method => :delete, :class => 'btn btn-mini btn-danger'
+    link_to '<i class="icon-trash"></i> '.html_safe, path, :confirm => 'Are you sure?', :method => :delete, :class => 'btn btn-mini btn-danger'
   end
   
   def cancel_link(path)
