b7ebb5a45b9fb8fef1d9396cd32bb1c490ab7dc5
gk0r <gk0r.go@gmail.com>
2012-08-29 16:32:15 +1000

    Removed Devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,4 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :token_authenticatable, :confirmable,
-  # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-
   # Setup accessible (or protected) attributes for your model
   attr_accessible :first_name, :last_name, :telephone_number, :role_id, :team_id, :email, :password, :password_confirmation, :remember_me
   belongs_to :role
@@ -15,7 +9,7 @@ class User < ActiveRecord::Base
   has_many :tech_specs
   has_many :code
   
-  validates_presence_of :first_name, :last_name, :telephone_number, :role_id, :team_id, :email
+  validates_presence_of :email, :first_name, :last_name, :telephone_number, :role_id, :team_id
   
   def name
     [first_name, last_name].join(' ')
--- db/schema.rb
+++ db/schema.rb
@@ -137,27 +137,14 @@ ActiveRecord::Schema.define(:version => 20120829032105) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "username"
     t.string   "first_name"
     t.string   "last_name"
+    t.string   "email"    
     t.string   "telephone_number"
     t.string   "role_id"
     t.string   "team_id"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          :default => 0
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
   end
-
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
-  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
 end
