c98228ed8a9190fd5f67f8886aa98aa02fefee92
gk0r <gk0r.go@gmail.com>
2012-09-08 20:46:56 +1000

    Enhanced DataTables functionality to allow different save states

--- app/assets/javascripts/datatables_conf.js
+++ app/assets/javascripts/datatables_conf.js
@@ -1,11 +1,25 @@
 // 
 // Data Tables configuration
 // 
+// $(function() {
+// 	$('table').dataTable(
+// 		{
+// 			"sDom": 'C<"row">Rlfrtip',
+// 			"bStateSave": true
+// 		});
+// });
+
 $(function() {
 	$('table').dataTable(
 		{
 			"sDom": 'C<"row">Rlfrtip',
-			"bStateSave": true
+			"bStateSave": true,
+			"fnStateSave": function (oSettings, oData) {
+			            localStorage.setItem( 'DataTables_'+window.location.pathname, JSON.stringify(oData) );
+       },
+       "fnStateLoad": function (oSettings) {
+           return JSON.parse( localStorage.getItem('DataTables_'+window.location.pathname) );
+       }
 		});
 });
 
