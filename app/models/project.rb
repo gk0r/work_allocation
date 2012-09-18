c67f9fc371bce4b7ccd2607efcf93726be03dd2c
gk0r <gk0r.go@gmail.com>
2012-09-18 16:51:03 +1000

    Misc updates

--- app/models/project.rb
+++ app/models/project.rb
@@ -10,6 +10,18 @@ class Project < ActiveRecord::Base
   has_many :software_releases,  :through    => :release_projects
   
   validates_presence_of :name
-  validates_uniqueness_of :dmr
+  validates_uniqueness_of :dmr, :if => :dmr?
   
+  attr_accessible :name, :dmr, :admin_ref, :category, :work_plan_id, :approval_status
+  
+  # def to_label
+  #   "DMR #{self.dmr} #{self.name}"
+  # end
+  
+  def dmr
+    # Allow blank DMR for future work that has not been assigned a formal record yet. Display "????" where there is no DMR recorded
+    dmr = read_attribute(:dmr)
+    dmr.empty? ? "????" : dmr if dmr
+  end
+    
 end
