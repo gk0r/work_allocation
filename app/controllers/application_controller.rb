d8e4fb67c636fd8ec94f45e9f35825bd0e783c70
gk0r <gk0r.go@gmail.com>
2012-09-20 14:57:33 +1000

    Handle ActiveRecord::RecordNotFound when switching between various development versions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,7 +20,7 @@ class ApplicationController < ActionController::Base
   protected
   
   def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
   end
   
   def user_signed_in?
