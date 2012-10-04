882b8c83d13775b810678657f510b5f8edeb1cdd
gk0r <gk0r.go@gmail.com>
2012-10-04 17:16:51 +1000

    SQL Tweaks to Deliverable model

--- app/models/deliverable.rb
+++ app/models/deliverable.rb
@@ -5,11 +5,14 @@ class Deliverable < ActiveRecord::Base
   has_many :overheads   , :dependent  => :destroy
 
   belongs_to :milestone  
-  belongs_to :user
   belongs_to :team
 
   has_one :project,           :through => :milestone
-  has_one :software_release,  :through => :software_release
+  has_one :software_release,  :through => :milestone
+
+  has_many :users, :through => :ba_specs
+  has_many :users, :through => :tech_specs
+  has_many :users, :through => :code
 
   accepts_nested_attributes_for :ba_specs
   accepts_nested_attributes_for :tech_specs
@@ -22,7 +25,7 @@ class Deliverable < ActiveRecord::Base
   def self.my_team(current_user)
     # Only display Deliverable Components that are assigned to user's team. 
     if !current_user.nil?
-      where(:deliverables => {:team_id => current_user.team_ids}).includes(:milestone, :ba_specs, :project, :tech_specs, :code, :overheads, :team)
+      where(:deliverables => {:team_id => current_user.team_ids}).includes(:milestone, :ba_specs, :project, :tech_specs, :code, :overheads, :team, :users, :software_release)
     else
       # Display everything to users who are not signed in as we cannot determine what their team is.
       # At some point - I should restrict the application such that everyone has to be logged in.
